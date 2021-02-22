9a4c0522a41ece46ba95d7c2bc1c73aa62dac90c
sarah <sschmidt0@uoc.edu>
2021-02-22 15:40:46 +0100

    nivell 1 - exercici 2

--- index.js
+++ index.js
@@ -30,3 +30,19 @@ console.log(toCelsius(59));
 console.log(padZeros('23', '5'));
 console.log(power(2, 3));
 console.log(greet('Sarah'));
+
+
+// Exercici 2 (not sure if I understood the exercise, I suppose we need an array with just the firstnames from the array users)
+const users = [
+  { firstName: 'Homer', lastName: 'Simpson' },
+  { firstName: 'Marge', lastName: 'Simpson' },
+  { firstName: 'Bart', lastName: 'Simpson' },
+  { firstName: 'Lisa', lastName: 'Simpson' },
+  { firstName: 'Maggie', lastName: 'Simpson' }
+];
+
+let usersFirstName = users.map(user => {
+  return user.firstName;
+});
+
+console.log(usersFirstName);
